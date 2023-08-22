01f3b22d61ec97ba6476a251a4d76ac5a2a929de
Sam-d23 <samkiragu05@gmail.com>
2023-08-22 22:31:31 +0300

    Task zero

--- main.h
+++ main.h
@@ -29,7 +29,7 @@ int (*func)(va_list, char[], int, int, int, int);
  * @fom: the format
  * @fom_t: the corresponding function
  */
-typedef sturct fom fom_t;
+typedef struct fom fom_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fom, int *j, va_list list, char buffer[],
 int flags, int width, int precision, int size);
@@ -53,7 +53,7 @@ int print_hexadecimal(va_list types, char buffer[], int flags,
 int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[], int flags,
 int width, int precision, int size);
-int print_hexa(va_list types, char map to[], char buffer[], int flags,
+int print_hexa(va_list types, char map_to[], char buffer[], int flags,
 char flag_ch, int width, int precision, int size);
 /* function that prints non-printable characters */
 int print_non_printable(va_list types, char buffer[],
@@ -79,7 +79,7 @@ int precision, int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsigned(int is_negative, int ind, char buffer[],
-int flags, intwidth, int precision, int size);
+int flags, int width, int precision, int size);
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
